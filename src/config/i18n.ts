abacec65fdd2a51a51dfe1151ae05286b279832b
RhiannaReid <rhianna.reidmckittrick@kainos.com>
2025-10-08 15:14:23 +0100

    added translation feature

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,7 +1,7 @@
+import path from "node:path";
 import i18next from "i18next";
 import Backend from "i18next-fs-backend";
 import * as middleware from "i18next-http-middleware";
-import path from "node:path";
 
 i18next
   .use(Backend)
