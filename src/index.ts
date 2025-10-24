ae1b44ccb9208c5a7a20fbb7bd9e5cb6ccea6f87
CarterrRyan <carter.ryan.todd@kainos.com>
2025-10-24 10:42:15 +0100

    fixed analytics

--- src/index.ts
+++ src/index.ts
@@ -64,6 +64,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
+app.use((_req, res, next) => {
+  res.locals.env = {
+    GA4_MEASUREMENT_ID: process.env.GA4_MEASUREMENT_ID,
+    NODE_ENV: process.env.NODE_ENV,
+  };
+  next();
+});
+
 // Add i18n middleware
 app.use(i18nextHandle(i18next));
 
