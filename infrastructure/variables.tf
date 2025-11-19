31901f6c06cc05f9fe3320f0ae6205e375ac89c4
CarterrRyan <carter.ryan.todd@kainos.com>
2025-11-19 10:53:11 +0000

    fixed var

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "team1-job-app-rg"
+  default     = "team1-job-app-frontend-rg"
 }
 
 variable "location" {
